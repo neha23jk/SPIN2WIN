a4cdb7d41cbb742a78f2a6ec361120d5315c848b
neha23jk <neha23jk@gmail.com>
2026-01-12 19:43:02 +0530

    Clean up

--- backend/models/User.js
+++ backend/models/User.js
@@ -54,7 +54,7 @@ const userSchema = new mongoose.Schema({
   timestamps: true
 });
 
-// Hash password before saving
+
 userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
   
@@ -67,12 +67,12 @@ userSchema.pre('save', async function(next) {
   }
 });
 
-// Compare password method
+
 userSchema.methods.comparePassword = async function(candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
 };
 
-// Remove password from JSON output
+
 userSchema.methods.toJSON = function() {
   const user = this.toObject();
   delete user.password;
